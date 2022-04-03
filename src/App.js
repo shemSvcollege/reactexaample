c737ef0a2ab0c5d7e8af56ed1fff8cd007701c49
shem bar <shem@svcollege.co.il>
2022-04-03 20:26:49 +0300

    react deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+//import {HashRouter} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO WORLD!!!
+    </div>
+  );
+}
+
+export default App;
